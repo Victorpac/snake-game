0028e309731ff5545c06aea780e24bd6d6f8b9d6
Victor <victorpac2019@gmail.com>
2024-02-22 13:41:16 +0100

    version 1.1: added preferences. Refactoring.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,80 @@
-const canvas = document.getElementById("gameField");
-const newGameButton = document.querySelector("#newGameButton");
+const DOMElements = {
+  canvas:               document.getElementById("gameField"),
+  newGameButton:        document.querySelector("#newGameButton"),
+  gameSettingsThumb:    document.querySelectorAll("[class^='settings-menu__']"),
+  snakeSizeField:       document.querySelector(".settings-menu__gridSize .toggle__field"),
+  gameSpeedField:       document.querySelector(".settings-menu__gameSpeed .toggle__field"),
+  gameScoreCounter:     document.querySelector("#scoreCounter"),
+  gameRecordScore:      document.querySelector("#recordScore"),
+};
 
-const ctx = canvas.getContext("2d");
+const preferences = {
+  gridSize: 20,
+  snakeSpeed: 20,
+  fieldWidth: DOMElements.canvas.clientWidth,
+  fieldHeight: DOMElements.canvas.clientHeight,
+  tileCount() {
+    return this.fieldWidth / this.gridSize
+  },
+  isTransparentBorders: false,
+};
 
-const gridSize = 20;
-const snakeSpeed = 10
-const fieldHeight = canvas.clientHeight;
-const fieldWidth = canvas.clientWidth;
-const tileCount = fieldWidth / gridSize;
+const game = {
+  score: 0,
+  recordScore: getCookie("score"),
+  isOver: false,
+  dx: 0,
+  dy: 0,
+};
 
-let isGameOver = false;
+const ctx = DOMElements.canvas.getContext("2d");
 
-let foodPosition = {
-  x: Math.floor(Math.random() * (tileCount - 1)),
-  y: Math.floor(Math.random() * (tileCount - 1)),
-}
-
-let snake = [{
-  x: tileCount / 2, // start position
-  y: tileCount / 2  // start position
-}]
+const settingFieldsList = {
+  "settings-menu__gameBorders": () => {
+    preferences.isTransparentBorders = !preferences.isTransparentBorders;
+  },
+  "settings-menu__gameSpeed": (e) => {
+    preferences.snakeSpeed = e.target.value;
+    gameOver();
+    newGame();
+  },
+  "settings-menu__gridSize": (e) => {
+    preferences.gridSize = e.target.value * 10;
+    gameOver();
+    newGame();
+  }
+};
 
 let gameTimer;
+let foodPosition = {
+  x: Math.floor(Math.random() * (preferences.tileCount() - 1)),
+  y: Math.floor(Math.random() * (preferences.tileCount() - 1)),
+};
 
-newGameButton.addEventListener("click", newGame);
+let snake = [{
+  x: preferences.tileCount() / 2, // start position
+  y: preferences.tileCount() / 2  // start position
+}];
+
+DOMElements.newGameButton.addEventListener("click", newGame);
 document.addEventListener("keydown", userControl);
 
-let dx = 0;
-let dy = 0;
+for (let el of DOMElements.gameSettingsThumb) {
+  el.addEventListener("change", updateSettings);
+}
+
+if (!game.recordScore) {
+  game.recordScore = 0;
+}
+
+DOMElements.snakeSizeField.value = preferences.gridSize / 10;
+DOMElements.gameSpeedField.value = preferences.snakeSpeed;
+
+DOMElements.gameScoreCounter.innerHTML = game.score;
+DOMElements.gameRecordScore.innerHTML = game.recordScore;
 
 function draw() {
-  ctx.clearRect(0, 0, fieldWidth, fieldHeight); // clear field before new frame
+  ctx.clearRect(0, 0, preferences.fieldWidth, preferences.fieldHeight); // clear field before new frame
 
   drawSnake();
   drawFood();
@@ -38,29 +82,40 @@ function draw() {
 }
 
 function drawSnake() {
-  ctx.fillStyle = "#111";
+  ctx.fillStyle = "#fff";
 
   for (let el of snake) {
-    ctx.fillRect(el.x * gridSize, el.y  * gridSize, gridSize, gridSize);
+    ctx.fillRect(el.x * preferences.gridSize, el.y  * preferences.gridSize, preferences.gridSize, preferences.gridSize);
   }
   const head = {
-    x: snake[0].x + dx,
-    y: snake[0].y + dy,
+    x: snake[0].x + game.dx,
+    y: snake[0].y + game.dy,
   }
   snake.unshift(head);
 }
 
 function collisionHandler() {
-  if (snake[0].x >= tileCount || snake[0].x < 0 || snake[0].y >= tileCount || snake[0].y < 0) {
-    gameOver();
-    return;
+  if (snake[0].x >= preferences.tileCount() || snake[0].x < 0 || snake[0].y >= preferences.tileCount() || snake[0].y < 0) {
+    if (!preferences.isTransparentBorders) {
+      gameOver();
+      return;
+    }else {
+      if (snake[0].x >= preferences.tileCount()) {
+        snake[0].x = 0;
+      } else if (snake[0].y >= preferences.tileCount()) {
+        snake[0].y = 0;
+      } else if (snake[0].x <= 0) {
+        snake[0].x = preferences.tileCount();
+      } else if (snake[0].y <= 0) {
+        snake[0].y = preferences.tileCount();
+      }
+    }
   }
 
   if (snake[0].x === foodPosition.x && snake[0].y === foodPosition.y) {
-    foodPosition = {
-      x: Math.floor(Math.random() * (tileCount - 1)),
-      y: Math.floor(Math.random() * (tileCount - 1)),
-    };
+    drawFood();
+    game.score++;
+    DOMElements.gameScoreCounter.innerHTML = game.score;
     return;
   }
 
@@ -73,57 +128,98 @@ function collisionHandler() {
 }
 
 function gameOver() {
-  isGameOver = true;
-  dy = 0;
-  dx = 0;
+  if (game.score > game.recordScore) {
+    document.cookie = `score=${game.score}`;
+    game.recordScore = getCookie("score");
+    DOMElements.gameRecordScore.innerHTML = game.recordScore;
+  }
+  game.isOver = true;
+  game.dy = 0;
+  game.dx = 0;
   document.body.classList.add("_gameOver");
   clearInterval(gameTimer);
 }
 
 function newGame() {
-  isGameOver = false;
+  game.isOver = false;
+  preferences.gameScore = 0;
+  DOMElements.gameScoreCounter.innerHTML = game.score;
+
   snake = [{
-    x: tileCount / 2, // start position
-    y: tileCount / 2,  // start position
+    x: preferences.tileCount() / 2, // start position
+    y: preferences.tileCount() / 2,  // start position
   }];
-  foodPosition = {
-    x: Math.floor(Math.random() * (tileCount - 1)),
-    y: Math.floor(Math.random() * (tileCount - 1)),
-  };
-  dy = 0;
-  dx = 0;
+
+  generateNewFoodPosition();
+
+  game.dy = 0;
+  game.dx = 0;
+
   document.body.classList.remove("_gameOver");
-  gameTimer = setInterval(draw, 1000 / snakeSpeed);
+  gameTimer = setInterval(draw, 1000 / preferences.snakeSpeed);
 }
 
 function drawFood() {
 
-  const radius = gridSize / 2;
+  const radius = preferences.gridSize / 2;
+
+  let snakeClone = snake.slice(1, snake.length);
+  if (snakeClone.find(el => el.x === foodPosition.x && el.y === foodPosition.y)) {
+    generateNewFoodPosition();
+    return;
+  }
 
   ctx.fillStyle = "red"
   ctx.beginPath();
-  ctx.arc(foodPosition.x * gridSize + radius, foodPosition.y * gridSize + radius, radius, 0, Math.PI * 2);
+  ctx.arc(foodPosition.x * preferences.gridSize + radius, foodPosition.y * preferences.gridSize + radius, radius, 0, Math.PI * 2);
   ctx.fill();
 }
 
+function generateNewFoodPosition() {
+  foodPosition = {
+    x: Math.floor(Math.random() * (preferences.tileCount() - 1)),
+    y: Math.floor(Math.random() * (preferences.tileCount() - 1)),
+  };
+}
+
 function userControl(event) {
-  if (event.key === "ArrowLeft" && dx !== 1) {
-    dx = -1;
-    dy = 0;
-  } else if (event.key === "ArrowRight" && dx !== -1) {
-    dx = 1;
-    dy = 0;
-  } else if (event.key === "ArrowDown" && dy !== -1) {
-    dx = 0;
-    dy = 1;
-  } else if (event.key === "ArrowUp" && dy !== 1) {
-    dx = 0;
-    dy = -1;
+  if (event.key === "ArrowLeft" && game.dx !== 1) {
+    game.dx = -1;
+    game.dy = 0;
+  } else if (event.key === "ArrowRight" && game.dx !== -1) {
+    game.dx = 1;
+    game.dy = 0;
+  } else if (event.key === "ArrowDown" && game.dy !== -1) {
+    game.dx = 0;
+    game.dy = 1;
+  } else if (event.key === "ArrowUp" && game.dy !== 1) {
+    game.dx = 0;
+    game.dy = -1;
   }
 
-  if (event.key === "Enter" && isGameOver) {
+  if (event.key === "Enter" && game.isOver) {
     newGame();
   }
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function updateSettings (e) {
+  settingFieldsList[e.currentTarget.classList](e);
 }
 
 newGame();
